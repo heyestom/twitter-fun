7595b6fbbdbd6b640de2d5fbb1103f16658bcd3e
Thomas Heyes <theyes@thoughtworks.com>
2016-04-17 18:51:20 +0530

    Groot twitter bot.

--- groot_on_twitter.py
+++ groot_on_twitter.py
@@ -1,4 +1,5 @@
-from  twitter import *
+import sys
+from twitter import *
 
 CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxx'
 CONSUMER_SECRET ='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
@@ -6,13 +7,26 @@ ACCESS_TOKEN =  'xxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 ACCESS_TOKEN_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
-ts = TwitterStream(auth=OAuth(CONSUMER_KEY, CONSUMER_SECRET,OAUTH_TOKEN, OAUTH_SECRET))
+ts = TwitterStream(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
+replies_index = 0
+replies = [	"I am Groot?","I am Groot!","I am Groot.",
+			"I AM GROOT!", "I am Groot??",
+			"I am Groot!!!", "I am Groot" ]
+
+# find any tweets which containt the word Groot
 groot_stream = ts.statuses.filter(track='Groot')
 
-for tweet in groot_stream:
-	tweet_id = tweet['id']
-	print 'incomming: ' + tweet['user']['screen_name'], tweet['text']
-	status = '@' + tweet['user']['screen_name'] + ' ' + 'I am Groot!'
-	print 'outgoing reply: ' + status
-	t.statuses.update(status=status, in_reply_to_status_id=tweet_id)
+for index, tweet in enumerate(groot_stream):
+  	#don't do retweets
+	if 'retweeted_status' not in tweet and "RT" not in tweet['text']:
+		# don't tweet at yourself!
+		if tweet['user']['screen_name'] != "___iamgroot___":
+			print "\nincomming: " + tweet['user']['screen_name'], tweet['text']
+			status = "@" + tweet['user']['screen_name'] + " " + replies[replies_index]
+			print "\noutgoing reply: " + status
+			t.statuses.update(status=status, in_reply_to_status_id=tweet['id'])
+
+			replies_index += 1
+			if replies_index >= len(replies):
+				sys.exit(0)
